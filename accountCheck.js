f8a6c5447be95a3571536500bd2e41af0f7fb249
hsiyu1121 <bluefish1121@gmail.com>
2020-08-12 19:38:05 +0800

    feat: add cookies function

--- accountCheck.js
+++ accountCheck.js
@@ -1,33 +1,40 @@
-function accountCheck(email, password){
-  const users = [{
-    firstName: 'Tony',
-    email: 'tony@stark.com',
-    password: 'iamironman'
-  },
-  {
-    firstName: 'Steve',
-    email: 'captain@hotmail.com',
-    password: 'icandothisallday'
-  },
-  {
-    firstName: 'Peter',
-    email: 'peter@parker.com',
-    password: 'enajyram'
-  },
-  {
-    firstName: 'Natasha',
-    email: 'natasha@gamil.com',
-    password: '*parol#@$!'
-  },
-  {
-    firstName: 'Nick',
-    email: 'nick@shield.com',
-    password: 'password'
-  }]
+function accountCheck(array){
+  const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+  ]
 
+  let name = ''
+  let email = ''
   for(let i = 0; i < users.length; i++) {
-    if(users[i].email === email && users[i].password === password ){
-      return  users[i].firstName
+    if(users[i].email === array.email && users[i].password === array.password ){
+      name = users[i].firstName
+      // email = users[i].email
+
+      return  name //, email
     }
   }
   return '帳號密碼錯誤，重新輸入！！'
--- app.js
+++ app.js
@@ -2,29 +2,49 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const accountCheck = require('./accountCheck')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser('123456789'))
 app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
-
+app.use(cookieParser('123'))
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const email = req.body.email
+  const array = req.body
+  const loginCheck = accountCheck(array)
+  if(req.signedCookies.login){
+    let loginCheck = req.signedCookies.login
+    res.render('login', {loginCheck})
+  }else {
+    res.render('index', {email})
+  }
 })
 
 app.post('/', (req, res) => {
+  const array = req.body
+  const loginCheck = accountCheck(array)
   const email = req.body.email
-  const password = req.body.password
-  const loginCheck = accountCheck(email, password)
-
+  const name = req.body.name
+  
   if(loginCheck.includes('帳號密碼錯誤')) {
-    res.render('index', {loginCheck, email, password})
+    res.render('index', {loginCheck, email})
   }else{
-    res.render('login', {loginCheck, email, password})
+    res.cookie('login', loginCheck, {path:'/', httpOnly: true, signed: true, maxAge: 60000}) //set cookie
+    res.render('login',  {loginCheck, email, name}) 
   }
 })
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('login', { path: '/' })
+  return res.redirect('/')
+})
+
 
 app.listen(port, () => {
   console.log(`Express is running on localhost:${port}`);
